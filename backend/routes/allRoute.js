e4796442ece5899a83c04e5f693ce8f82b968cc6
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-14 00:43:50 +0800

    update

--- backend/routes/allRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-
-router.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-});
-
-module.exports = router;
