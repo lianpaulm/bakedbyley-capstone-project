8823d32071fd8a0d75d77638f2770e939d2e377b
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-14 00:36:38 +0800

    update

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('frontend/build'));
 
-  app.use(all);
+  app.use('*', all);
 
   // app.get('*', (req, res) => {
   //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
