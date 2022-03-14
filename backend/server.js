fae0d46f29ba126016a2098f2ecca142cb95f26c
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-15 00:00:42 +0800

    i hope

--- backend/server.js
+++ backend/server.js
@@ -28,17 +28,26 @@ app.use(notFound);
 app.use(errorHandlerMiddleware);
 
 // serve static assets if in production
-if (process.env.NODE_ENV == 'production') {
-  app.use(express.static('../frontend/build'));
-  // set static folder
+// if (process.env.NODE_ENV == 'production') {
+//   app.use(express.static('../frontend/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   });
+// }
+
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
   });
 }
 
 // connecting to db before to start server
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
