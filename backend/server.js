4ccfaf30fd5956498a7bdf0354b2586eb6b5d4b0
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-14 23:31:30 +0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,7 @@ const user = require('./routes/userRoutes');
 const config = require('./routes/configRoute');
 // db
 const connectDB = require('./db/connect');
-if (process.env.NODE_ENV !== 'PRODUCTION') {
-  require('dotenv').config();
-}
+require('dotenv').config();
 // middleware
 const notFound = require('./middleware/notFound');
 const errorHandlerMiddleware = require('./middleware/errorHandler');
@@ -28,13 +26,16 @@ app.use('/api/v1/config', config);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
+// serve static assets if in production
+console.log(process.env.NODE_ENV === 'production');
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('../frontend/build'));
 
-// set static folder
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 // connecting to db before to start server
 const port = process.env.PORT || 5000;
@@ -48,4 +49,5 @@ const start = async () => {
     console.log(error);
   }
 };
+
 start();
