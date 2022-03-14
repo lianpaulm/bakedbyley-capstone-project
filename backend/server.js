3dc10699f42bd26f32794fa80c44fbcc63a46ea0
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-14 22:29:13 +0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
     res.send('API is running');
   });
 }
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
 
 // connecting to db before to start server
 const port = process.env.PORT || 5000;
