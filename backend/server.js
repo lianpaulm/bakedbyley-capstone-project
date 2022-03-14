865a8c67fe4b118da62657279b1f9bcb8842b41b
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-14 21:21:37 +0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,10 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
 }
 
 // connecting to db before to start server
