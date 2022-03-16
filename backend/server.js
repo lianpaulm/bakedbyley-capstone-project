1982ae4de61c5b6e4cfb135601536864b85108ed
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-16 11:17:40 +0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,14 @@ app.use('/api/v1/config', config);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
+
 // connecting to db before to start server
 const port = process.env.PORT || 5000;
 const start = async () => {
@@ -41,13 +49,5 @@ const start = async () => {
     console.log(error);
   }
 };
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
-  });
-}
 
 start();
