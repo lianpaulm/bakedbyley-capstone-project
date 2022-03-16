64a357dbfe479d3b840660c26ec50411e7992205
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-16 17:33:40 +0800

    deleting cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
-app.use(cors());
 
 // router
 const products = require('./routes/productsRoute');
