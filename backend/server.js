e6ea94fa2349aa4b3cee206fbd9ac7fb4a083770
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-16 10:10:59 +0800

    fixing some bugs

--- backend/server.js
+++ backend/server.js
@@ -29,14 +29,6 @@ app.use('/api/v1/config', config);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
-  });
-}
-
 // connecting to db before to start server
 const port = process.env.PORT || 5000;
 const start = async () => {
@@ -49,5 +41,13 @@ const start = async () => {
     console.log(error);
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
 
 start();
