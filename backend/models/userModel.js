dfaafc380310c29111680677df3fc5138a8dca67
Lian Paul <lianpaulmolo08@gmail.com>
2022-03-26 00:17:22 +0800

    Editing system guide text

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -12,13 +12,12 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please enter your email'],
-    unique: true,
     validate: [validator.isEmail, 'Please enter a valid email'],
   },
   password: {
     type: String,
     required: [true, 'Please enter your password'],
-    minlength: [8, 'Password should be greater than 8 characters'],
+    // minlength: [8, 'Password should be greater than 8 characters'],
     select: false,
   },
   role: {
